54b26e70dbf0c9e7fd20badf1245c616de15bd32
Pinkal Raval <pinkal@ashutec.com>
2022-10-14 15:37:45 +0530

    Creating enable dark mode button using useState Hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,5 +32,5 @@ export default function Navbar(props) {
 Navbar.propTypes = { title: PropTypes.string.isRequired, aboutText: PropTypes.string.isRequired };
 Navbar.defaultProps = {
    title: 'Set title here',
-   aboutText: 'About text here'
+   aboutText: 'About'
   };
