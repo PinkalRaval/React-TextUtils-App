07feb7b7d46aee36bcb6e12e0d0504be600829da
Pinkal Raval <pinkal@ashutec.com>
2022-10-17 10:46:58 +0530

    Changing title dynamically & adding favicons to TextUtils

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Alert from './components/Alert';
 function App() {
   const [mode, setMode] = useState('light') // Whether dark mode is enabled or not
@@ -16,26 +16,28 @@ function App() {
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-    
+
   }
   const toggleMode = () => {
-    if(mode === 'light') {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled ", "success");
+      document.title = 'TextUtils - Dark Mode';
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white ';
       showAlert("Light mode has been enabled ", "success");
+      document.title = 'TextUtils - Light Mode';
     }
   }
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
       {/* <Navbar /> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
         {/* <About /> */}
